01d5215890fc51fba31c4edae5535858d23976c1
FejKer <ozikleuszek@gmail.com>
2021-12-28 01:05:46 +0100

    added more functions

---
+++ checkMove.c
@@ -0,0 +1,6 @@
+int checkColumn(char array[6][7], int column) {
+    if(array[0][column] != 0) {
+        return 0;
+    }
+    return 1;
+}
--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <time.h>
-#include <pthread.h>
+//#include <pthread.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int sec = 0;
 int min = 0;
@@ -16,8 +17,20 @@ void *timer(void *threadid){
     }
 }
 
-void printField(char array[6][7]){
-
+void printField(char array[6][7], char* name1, char* name2, int round){
+    system("clear");
+    printf("Tura gracza: ");
+    if(round % 2 == 1) {
+        printf("%s\n", name1);
+    } else {
+        printf("%s\n", name2);
+    }
+    for(int i = 0; i < 6; i++){
+        for(int j = 0; j < 7; j++){
+            printf("%c ", array[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 void printMenu(){
@@ -34,25 +47,38 @@ void printMenu(){
     }
 }
 
-int gameEnd() {                                 //funkcja sprawdzajaca stan gry po kazdym ruchu
-
-
-    return 0;
-}
-
 int main() {
+    int round = 1;
+    int column;
+    char name1[255];
+    char name2[255];
     int sec = 0, min = 0;
-    pthread_t thread_id;                        //deklaracja id wątku
     FILE *fptr;
     fptr = fopen("data.bin", "ab+");            //plik z danymi o graczach i wynikach
     char array[6][7];                           //tablica charow, zeby byl czytelnieszy output
     for (int i = 0; i < 6; i++){
         for(int j = 0; j < 7; j++){             //zerowanie tablicy
-            array[i][j] = 177;
+            array[i][j] = 0;
         }
     }
     printMenu();
-    pthread_create(&thread_id, NULL, timer, NULL);          //rozpoczecie watku z timerem
+    
+    printf("Podaj imie gracza 1.\n");
+    scanf("%s", name1);
+    printf("Podaj imie gracza 2.\n");
+    scanf("%s", name2);
+    
+    while(1){
+        printField(array, name1, name2, round);
+        printf("\n");
+        printf("\n");
+        printf("Podaj numer kolumny do ktorej chcesz wrzucic zeton.\n");
+        scanf("%d", column);
+        round++;
+    }
+    // pthread_t thread_id; 
+    // pthread_create(&thread_id, NULL, timer, NULL);          //rozpoczecie watku z timerem do zrobienia
+
 
 
     return 0;
