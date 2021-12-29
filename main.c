252e6ceaee17fe2efcdbebe14bb7d53903a10926
Migowski Oskar <oskar.migowski@polpharma.com>
2021-12-29 03:26:03 +0100

    moved print functions to separate file

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #include "checkMove.c"
 #include "gameEnd.c"
 #include "undoMove.c"
+#include "print.c"
 
 int sec = 0;
 int min = 0;
@@ -21,73 +22,6 @@ int min = 0;
 //    }
 //}
 
-void printField(char array[6][7], char* name1, char* name2, int round, int wrong, int wrongCommand, int failedUndo){
-    system("clear");
-    system("cls");
-    printf("Tura gracza: ");
-    if(round % 2 == 1) {
-        printf("%s\n", name1);
-    } else {
-        printf("%s\n", name2);
-    }
-    printf("Tura numer: %d\n", round);
-
-
-    for(int i = 0; i < 6; i++){
-        for(int j = 0; j < 7; j++){
-            printf("%c ", array[i][j]);     //wypisanie pola gry
-        }
-        printf("\n");
-    }
-    if(wrong){
-        printf("\n");
-        printf("*************************************\n");
-        printf("Podana kolumna jest juz zajeta!!!\n");
-        printf("*************************************\n");
-    }
-    if(wrongCommand){
-        printf("\n");
-        printf("*************************************\n");
-        printf("Podano bledna komende!!!\n");
-        printf("*************************************\n");
-    }
-    if(failedUndo){
-        printf("\n");
-        printf("*************************************\n");
-        printf("Cofniecie bedzie dostepne po\n");
-        printf("nastepnym wykonanym ruchu!!!\n");
-        printf("*************************************\n");
-    }
-}
-
-void printMenu(){
-    int choice;
-    printf("1. Rozpoczecie gry\n");
-    printf("2. Wyswietlenie tabeli wynikow\n");
-    printf("3. Wyswietlanie danych gracza\n");
-    scanf("%d", &choice);
-    switch(choice){
-        case 1: return;
-        case 2: break;                          //do zrobienia listy
-        case 3: break;
-        default: system("clear"); system("cls"); printf("Podano bledna komende.\n"); printMenu();
-    }
-}
-
-int printMenuInGame(char array[6][7], int column, int round, int undo){
-    int choice;
-    printf("\n");
-    printf("1. Powrot do gry\n");
-    printf("2. Cofniecie ruchu\n");
-    printf("3. Powrot do menu glownego\n");
-    scanf("%d", &choice);
-    switch(choice){
-        case 1: return 0; break;
-        case 2: undoMove(array, column, round, undo); return 1; break;                          //do zrobienia listy
-        case 3: return 2; break;
-        default: system("clear"); system("cls"); printf("Podano bledna komende.\n"); printMenuInGame(array, column, round, undo);
-    }
-}
 
 int main() {
     system("cls");
