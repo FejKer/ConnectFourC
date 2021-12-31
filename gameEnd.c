bf23fe07c8a5f04903963f29d7666a9d7db65663
Migowski Oskar <oskar.migowski@polpharma.com>
2021-12-31 02:41:23 +0100

    cleaned code and made checkWinDiagonallyLeft function

--- gameEnd.c
+++ gameEnd.c
@@ -11,15 +11,23 @@ int checkDraw(char array[6][7]){
 
 int checkWin(char array[6][7], int round){
     if(checkWinHorizontal(array, round)){
+        printf("checkWinHorizontal\n");
+        sleep(2);
         return 1;
     }
     if(checkWinVertical(array, round)){
+        printf("checkWinVertical\n");
+        sleep(2);
         return 1;
     }
     if(checkWinDiagonallyLeft(array, round)){
+        printf("checkWinDiagonallyLeft\n");
+        sleep(2);
         return 1;
     }
     if(checkWinDiagonallyRight(array, round)){
+        printf("checkWinDiagonallyRight\n");
+        sleep(2);
         return 1;
     }
     return 0;
@@ -27,33 +35,21 @@ int checkWin(char array[6][7], int round){
 
 int checkWinHorizontal(char array[6][7], int round){
     int length = 0;
+    int seek;
     if(round % 2 == 1){
-        int seek = 99;
-        for(int i = 0; i < 6; i++){
-            for(int j = 0; j < 7; j++){
-                if(array[i][j] == seek){
-                    length++;
-                } else {
-                    length = 0;
-                }
-                if(length > 3) {
-                    return 1;
-                }
-            }
-        }
-
+        seek = 99;
     } else {
-        int seek = 122;
-        for(int i = 0; i < 6; i++){
-            for(int j = 0; j < 7; j++){
-                if(array[i][j] == seek){
-                    length++;
-                } else {
-                    length = 0;
-                }
-                if(length > 3) {
-                    return 1;
-                }
+        seek = 122;
+    }
+    for(int i = 0; i < 6; i++){
+        for(int j = 0; j < 7; j++){
+            if(array[i][j] == seek){
+                length++;
+            } else {
+                length = 0;
+            }
+            if(length > 3) {
+                return 1;
             }
         }
     }
@@ -61,33 +57,21 @@ int checkWinHorizontal(char array[6][7], int round){
 }
 int checkWinVertical(char array[6][7], int round){
     int length = 0;
+    int seek;
     if(round % 2 == 1){
-        int seek = 99;
-        for(int i = 0; i < 6; i++){
-            for(int j = 0; j < 7; j++){
-                if(array[j][i] == seek){
-                    length++;
-                } else {
-                    length = 0;
-                }
-                if(length > 3) {
-                    return 1;
-                }
-            }
-        }
-
+        seek = 99;
     } else {
-        int seek = 122;
-        for(int i = 0; i < 6; i++){
-            for(int j = 0; j < 7; j++){
-                if(array[j][i] == seek){
-                    length++;
-                } else {
-                    length = 0;
-                }
-                if(length > 3) {
-                    return 1;
-                }
+        seek = 122;
+    }
+    for(int i = 0; i < 6; i++){
+        for(int j = 0; j < 7; j++){
+            if(array[j][i] == seek){
+                length++;
+            } else {
+                length = 0;
+            }
+            if(length > 3) {
+                return 1;
             }
         }
     }
@@ -95,9 +79,105 @@ int checkWinVertical(char array[6][7], int round){
 }
 int checkWinDiagonallyLeft(char array[6][7], int round){
     int length = 0;
+    int seek;
+    if(round % 2 == 1){
+        seek = 99;
+    } else {
+        seek = 122;
+    }
+    for(int i = 2; i < 6;){
+        for(int j = 0; j <= 3; j++){
+            if(array[i][j] == seek){
+                length++;
+            } else {
+                length = 0;
+            }
+            if(length > 3){
+                return 1;
+            }
+                i++;
+        }
+    }
+    length = 0;
+
+    for(int i = 1; i < 6;){
+        for(int j = 0; j <= 4; j++){
+            if(array[i][j] == seek){
+                length++;
+            } else {
+                length = 0;
+            }
+            if(length > 3){
+                return 1;
+            }
+            i++;
+        }
+    }
+    length = 0;
+
+    for(int i = 0; i < 6;){
+        for(int j = 0; j <= 5; j++){
+            if(array[i][j] == seek){
+                length++;
+            } else {
+                length = 0;
+            }
+            if(length > 3){
+                return 1;
+            }
+            i++;
+        }
+    }
+    length = 0;
+
+    for(int i = 0; i < 6;){
+        for(int j = 1; j <= 6; j++){
+            if(array[i][j] == seek){
+                length++;
+            } else {
+                length = 0;
+            }
+            if(length > 3){
+                return 1;
+            }
+            i++;
+        }
+    }
+    length = 0;
+
+    for(int i = 0; i < 6;){
+        for(int j = 2; j <= 6; j++){
+            if(array[i][j] == seek){
+                length++;
+            } else {
+                length = 0;
+            }
+            if(length > 3){
+                return 1;
+            }
+            i++;
+        }
+    }
+    length = 0;
+
+    for(int i = 0; i < 6;){
+        for(int j = 3; j <= 6; j++){
+            if(array[i][j] == seek){
+                length++;
+            } else {
+                length = 0;
+            }
+            if(length > 3){
+                return 1;
+            }
+            i++;
+        }
+    }
+
     return 0;
 }
 int checkWinDiagonallyRight(char array[6][7], int round){
     int length = 0;
+
     return 0;
 }
